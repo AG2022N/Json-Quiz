8a359744ddaa562d3a5424b35080bcadf8bb9eb0
ag <ag@PROELE64>
2024-02-05 12:42:41 +0100

    Chanes

--- 2024-02-05_Json-Quiz/Program.cs
+++ 2024-02-05_Json-Quiz/Program.cs
@@ -1,36 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using Newtonsoft.Json.Converters;
-using System.IO;
-class Program
+﻿class Program
 {
     static void Main()
     {
         for (int i = 1; i < 5;)
         {
-            string filePath = (@"Questions/question.json");
-            Convert_json_to_cs(filePath);
+            string filePath = (@"Questions/Question.json");
+            //string filePath = (@"Questions/Question{0}.json", i.ToString());
+            Convert_json_to_object(filePath);
             //Console.WriteLine(q.question);
             //string userAnswer = Console.ReadLine();
-            //if userAnswer == q.correctAnswer , write q.correctAnswerExplanation
+            //if (userAnswer == q.correctAnswer)
+            //{
+            //    Console.WriteLine("Correct!");
+            //}
+            //console.writeline(q.correctAnswerExplanation)
+            Console.ReadKey();
+            Console.Clear();
             i++;
         }
     }
-    public class quizQuestion()
+    public class QuizQuestion()
     {
         public string question { get; set; }
         public string correctAnswer { get; set; }
         public string correctAnswerExplanation { get; set; }
     }
-    static void Convert_json_to_cs(string filePath)
+    static void Convert_json_to_object(string filePath)
     {
         string path = filePath;
         string convertStorage = File.ReadAllText(path);
-        quizQuestion q = Newtonsoft.Json.JsonConvert.DeserializeObject<quizQuestion>(convertStorage);
+        QuizQuestion q = Newtonsoft.Json.JsonConvert.DeserializeObject<QuizQuestion>(convertStorage);
     }
 }
