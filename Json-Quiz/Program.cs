773b74fa0e3635cdd66604ef68a040b4d49f3933
ag <ag@PROELE64>
2024-02-12 11:53:49 +0100

    Finished basically

--- Json-Quiz/Program.cs
+++ Json-Quiz/Program.cs
@@ -2,32 +2,30 @@
 {
     static void Main()
     {
-        for (int i = 1; i <= 5;)
+        for (int i = 1; i <= 6;)
         {
             string Filepath = (@"Questions/Question" + i + ".json");
-            QuizQuestion CurrentQuestion = new QuizQuestion();
+            string ConvertStorage = File.ReadAllText(Filepath);
+            QuizQuestion CurrentQuestion = Newtonsoft.Json.JsonConvert.DeserializeObject<QuizQuestion>(ConvertStorage);
             Console.WriteLine(CurrentQuestion.Question);
+            Console.WriteLine(CurrentQuestion.AnswerOptions);
+            Console.Write("\nIndtast: ");
             string UserAnswer = Console.ReadLine();
             if (UserAnswer == CurrentQuestion.CorrectAnswer)
             {
-                Console.WriteLine("Correct!");
+                Console.WriteLine("Korrekt!");
             }
-            Console.WriteLine(CurrentQuestion.CorrectAnswerExplanation);
+            Console.Write(CurrentQuestion.CorrectAnswerExplanation);
             Console.ReadKey();
-            Console.Clear();
+            Console.WriteLine("\n");
             i++;
         }
     }
     public class QuizQuestion()
     {
         public string Question { get; set; }
+        public string AnswerOptions { get; set; }
         public string CorrectAnswer { get; set; }
         public string CorrectAnswerExplanation { get; set; }
-    }
-    static void JsonToObject(string Filepath)
-    {
-        string Path = Filepath;
-        string ConvertStorage = File.ReadAllText(Path);
-        QuizQuestion q = Newtonsoft.Json.JsonConvert.DeserializeObject<QuizQuestion>(ConvertStorage);
     }
 }
